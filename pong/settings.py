f2ccf1a07236de749e12a22058fe069308d5a18b
AveePB <94467965+AveePB@users.noreply.github.com>
2023-08-08 17:57:24 +0200

    Create settings.py

---
+++ pong/settings.py
@@ -0,0 +1,22 @@
+WIDTH = 700
+HEIGHT = 500
+TITLE = "Pong"
+MAX_FRAMERATE = 60
+ICON_PATH = "./assets/img/icon.jpg"
+
+BACKGROUND_COLOR = (0, 0, 0) #black
+FOREGROUND_COLOR = (255, 255, 255) #white
+
+PADDLE_SPEED = 4
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+PADDLE_COLOR = (255, 255, 255) #white
+
+BALL_COLOR = (255, 0, 0) #white
+BALL_RADIUS = 10
+BALL_SPEED = 5
+
+FONT_PATH = "./assets/font/press-start-2p.ttf"
+FONT_SIZE = 30
+
+MAX_SCORE = 10
